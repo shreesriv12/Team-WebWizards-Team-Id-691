4de11e35ed2e18411ad6e4e0be512611edf33690
shreesriv12 <shreeyasrivastava1124@gmail.com>
2024-11-06 22:50:24 +0530

    Delete route directory

--- route/Todo.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router()
-import {To_do} from "../controller/Todo.controller.js"
-
-
-router.post("/create",To_do);
-export default router; 
-
-router.post
