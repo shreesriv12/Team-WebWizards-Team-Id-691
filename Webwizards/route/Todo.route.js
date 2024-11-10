ad8116a2e31b2cefb0f376cf8a5d10546109baa7
Venu4i <venuverma04@gmail.com>
2024-11-10 08:43:54 +0530

    Update Todo.route.js

--- Webwizards/route/Todo.route.js
+++ Webwizards/route/Todo.route.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import path from 'path';
 const router = express.Router()
-import {To_do} from "../controller/Todo.controller.js"
+import {To_do} from "../controller/Todo.controller.js";
+import { cwd } from 'process';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+router.get('/', (req, res) => {
+    const htmlFilePath = path.join(process.cwd(), 'public', 'todolist.html');  // Get absolute path
+    res.sendFile(htmlFilePath, (err) => {
+      if (err) {
+        res.status(500).send('Error in sending the HTML file');
+      }
+    });
+  });
 
 
-router.post("/create",To_do);
+router.post("/",To_do);
 export default router; 
-
-router.post
