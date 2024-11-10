7a70bd91600c520339b34cbcd419f767ca21947d
Venu4i <venuverma04@gmail.com>
2024-11-10 08:34:10 +0530

    Update Todo.js

--- Webwizards/model/Todo.js
+++ Webwizards/model/Todo.js
@@ -4,11 +4,10 @@ import express from "express";
 
 const Todo_listSchema = new mongoose.Schema({
     Task_Title:{type: String, required:true},
-    Description:{type:String},
-    Label:{type:String},
     Deadline:{type:Date},
+    Description:{type:String},
+    Genre:{type:String},
    Priority:{type:String, required:true},
-   Done:{type:Boolean},
 
 });
 const Todo = mongoose.model("Todo",Todo_listSchema);
